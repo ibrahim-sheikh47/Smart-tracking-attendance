55d65bd3a28086a60b3f57e0daff5522c555ec18
Ali Lal Din <alilaldin7@gmail.com>
2025-04-30 16:18:03 +0500

    Daddy is daddy.

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 // Cloud function to create a new employee
-exports.createNewEmployee = functions.https.onCall(async (data, context) => {
+exports.createNewEmployee = functions.https.onCall(async (response) => {
     // Check if the request is from an authenticated admin
     // if (!context.auth) {
     //     throw new functions.https.HttpsError(
@@ -16,36 +16,46 @@ exports.createNewEmployee = functions.https.onCall(async (data, context) => {
     // if (!context.auth.token.admin) {
     //   throw new functions.https.HttpsError("permission-denied", "Only admins can create employees");
     // }
-
+    // Check if data is properly received
+    console.log("Data from createNewEmployee():", response.data);
+    //
+    // if (!data || !data.email) {
+    //     console.error("Invalid data received:", data);
+    //     throw new functions.https.HttpsError(
+    //         "invalid-argument",
+    //         "The function must be called with valid employee data."
+    //     );
+    // }
+    console.log("Email of new user:", response.data.email);
     try {
         const userRecord = await admin.auth().createUser({
-            email: "helloali@gmail.com",
-            password: "123123", // Using your static password
-            //displayName: `${data.firstName} ${data.lastName}`,
-            // photoURL: data.profileImageUrl || '',
+            email: response.data.email,
+            password: "123123123", // Using your static password
+            displayName: `${response.data.firstName} ${response.data.lastName}`
         });
 
+        console.log("User created successfully:", userRecord.uid);
 
         // Set custom claims for role-based access
         await admin.auth().setCustomUserClaims(userRecord.uid, {
             role: 'employee',
-            department: data.department
+            department: response.data.department
         });
 
         // Add user data to Firestore (using the same structure as your current code)
         await admin.firestore().collection('employees').doc(userRecord.uid).set({
             uid: userRecord.uid,
-            firstName: "Ali",
-            lastName: "Lal Din",
-            department: "Management",
-            bio: "Hello I am a well know s beast.",
-            email: "helloworld@gmail.com",
-            phoneNumber: "03316714994",
-            designation: "Bed Master",
-            hourlyRate: "6",
-            workingHours: "9",
-            overtimeRate: "10",
-            // profileImageUrl: data.profileImageUrl || "",
+            firstName: response.data.firstName,
+            lastName: response.data.lastName,
+            department: response.data.department,
+            bio: response.data.bio || "",
+            email: response.data.email,
+            phoneNumber: response.data.phoneNumber,
+            designation: response.data.designation,
+            hourlyRate: Number(response.data.hourlyRate || 0),
+            workingHours: Number(response.data.workingHours || 0),
+            overtimeRate: Number(response.data.overtimeRate || 0),
+            //profileImageUrl: data.profileImageUrl || "",
             //createdAt: admin.firestore.FieldValue.serverTimestamp(),
         });
 
@@ -55,7 +65,8 @@ exports.createNewEmployee = functions.https.onCall(async (data, context) => {
             uid: userRecord.uid,
             message: "Employee added successfully"
         };
-    } catch (error) {
+    }
+    catch (error) {
         console.error("Error creating new employee:", error);
         throw new functions.https.HttpsError("internal", error.message);
     }
